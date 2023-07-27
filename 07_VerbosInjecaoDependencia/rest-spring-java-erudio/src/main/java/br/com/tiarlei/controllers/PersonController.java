0d0c0145879d1b5cdb7a128a201a196395826b95
TiarleiNoeremberg <tiarlei.com@gmail.com>
2023-07-26 21:12:21 -0300

    Implementando endpoint findAll com dados mocados

--- 07_VerbosInjecaoDependencia/rest-spring-java-erudio/src/main/java/br/com/tiarlei/controllers/PersonController.java
+++ 07_VerbosInjecaoDependencia/rest-spring-java-erudio/src/main/java/br/com/tiarlei/controllers/PersonController.java
@@ -1,5 +1,7 @@
 package br.com.tiarlei.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,12 +19,17 @@ public class PersonController {
 	@Autowired
 	private PersonServices service;
 		
+	@RequestMapping(method=RequestMethod.GET,
+			produces = MediaType.APPLICATION_JSON_VALUE)
+	public List<Person> findAll() {
+		return service.findAll();
+	}
+	
 	@RequestMapping(value = "/{id}",
 			method=RequestMethod.GET,
 			produces = MediaType.APPLICATION_JSON_VALUE)
 	public Person findPersonById(
-			@PathVariable(value = "id") String id) throws Exception{
-				
+			@PathVariable(value = "id") String id) {
 		return service.findById(id);
 	}
 	
--- 07_VerbosInjecaoDependencia/rest-spring-java-erudio/src/main/java/br/com/tiarlei/services/PersonServices.java
+++ 07_VerbosInjecaoDependencia/rest-spring-java-erudio/src/main/java/br/com/tiarlei/services/PersonServices.java
@@ -1,5 +1,8 @@
 package br.com.tiarlei.services;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
 import java.util.logging.Logger;
 
@@ -13,6 +16,18 @@ public class PersonServices {
 	private final AtomicLong counter = new AtomicLong();
 	private Logger logger = Logger.getLogger(PersonServices.class.getName());
 	
+	public List<Person> findAll() {
+		
+		logger.info("Finding all people!");
+		
+		List<Person> persons = new ArrayList<>();
+		for (int i = 0; i < 8; i++) {
+			Person person = mockPerson(i);
+			persons.add(person);
+		}
+		return persons;
+	}
+		
 	public Person findById(String id) {
 		
 		logger.info("Finding one person!");
@@ -23,8 +38,18 @@ public class PersonServices {
 		person.setLastName("Noeremberg");
 		person.setAddress("Florianópolis - SC - Brasil");
 		person.setGender("Male");
-				
 		return person;
+	}
+	
+	private Person mockPerson(int i) {
+		
+	Person person = new Person();
+	person.setId(counter.incrementAndGet());
+	person.setFirstName("Person name " + i);
+	person.setLastName("Last name " + i);
+	person.setAddress("Some address in Brasil " + i);
+	person.setGender("Male");// TODO Auto-generated method stub
+	return person;
 	}
 
 }
