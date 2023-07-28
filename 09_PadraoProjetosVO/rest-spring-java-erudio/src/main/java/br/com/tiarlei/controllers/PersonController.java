a7201c5f345c0c07c64b44660f281cb048a8568f
TiarleiNoeremberg <tiarlei.com@gmail.com>
2023-07-28 18:40:58 -0300

    Implementando Value Object e Dozer Mapper

--- 09_PadraoProjetosVO/rest-spring-java-erudio/src/main/java/br/com/tiarlei/controllers/PersonController.java
+++ 09_PadraoProjetosVO/rest-spring-java-erudio/src/main/java/br/com/tiarlei/controllers/PersonController.java
@@ -5,8 +5,16 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-import br.com.tiarlei.model.Person;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.tiarlei.data.vo.v1.PersonVO;
 import br.com.tiarlei.services.PersonServices;
 
 @RestController
@@ -17,13 +25,13 @@ public class PersonController {
 	private PersonServices service;
 		
 	@GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
-	public List<Person> findAll() {
+	public List<PersonVO> findAll() {
 		return service.findAll();
 	}
 	
 	@GetMapping(value = "/{id}",
             produces = MediaType.APPLICATION_JSON_VALUE)
-	public Person findById(
+	public PersonVO findById(
 			@PathVariable(value = "id") Long id) {
 		return service.findById(id);
 	}
@@ -31,14 +39,14 @@ public class PersonController {
 	@PostMapping(
 			consumes = MediaType.APPLICATION_JSON_VALUE,
 			produces = MediaType.APPLICATION_JSON_VALUE)
-	public Person create(@RequestBody Person person) {
+	public PersonVO create(@RequestBody PersonVO person) {
 		return service.create(person);
 	}
 	
 	@PutMapping(
 			consumes = MediaType.APPLICATION_JSON_VALUE,
 			produces = MediaType.APPLICATION_JSON_VALUE)
-	public Person update(@RequestBody Person person) {
+	public PersonVO update(@RequestBody PersonVO person) {
 		return service.update(person);
 	}
 	
