ecba3e5a2171c59a4f577782cedceb0cd571e1c5
TiarleiNoeremberg <tiarlei.com@gmail.com>
2023-08-05 13:46:39 -0300

    Implementando Swagger no projeto

---
+++ 15_Swagger/rest-spring-java-erudio/src/main/resources/db/migration/V1__Create_Table_Person.sql
@@ -0,0 +1,8 @@
+CREATE TABLE IF NOT EXISTS person
+(
+    id bigserial primary key,
+	first_name varchar(80) NOT NULL,
+	last_name varchar(80) NOT NULL,
+	address varchar(100) NOT NULL,
+	gender varchar(6) NOT NULL
+);
