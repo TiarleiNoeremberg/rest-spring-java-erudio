9489bebe9a2b380f276ee30b000803a191d669da
TiarleiNoeremberg <tiarlei.com@gmail.com>
2023-08-20 23:20:07 -0300

    Implementado em PersonControllerJsonTest os testes delete e findAll

--- 17_RestAssured_Testcontainers_JUnit5/rest-spring-java-erudio/src/test/java/br/com/tiarlei/integrationtests/controller/withjson/PersonControllerJsonTest.java
+++ 17_RestAssured_Testcontainers_JUnit5/rest-spring-java-erudio/src/test/java/br/com/tiarlei/integrationtests/controller/withjson/PersonControllerJsonTest.java
@@ -5,6 +5,8 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import java.util.List;
+
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
 import org.junit.jupiter.api.Order;
@@ -13,6 +15,7 @@ import org.junit.jupiter.api.TestMethodOrder;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -23,6 +26,7 @@ import br.com.tiarlei.integrationtests.testcontainers.AbstractIntegrationTest;
 import br.com.tiarlei.integrationtests.dto.AccountCredentialDTO;
 import br.com.tiarlei.integrationtests.dto.PersonDTO;
 import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.common.mapper.TypeRef;
 import io.restassured.filter.log.LogDetail;
 import io.restassured.filter.log.RequestLoggingFilter;
 import io.restassured.filter.log.ResponseLoggingFilter;
@@ -177,6 +181,68 @@ public class PersonControllerJsonTest extends AbstractIntegrationTest{
 			assertEquals("Brasília - DF - Brasil", persistedPerson.getAddress());
 			assertEquals("Male", persistedPerson.getGender());
 		}
+		
+		@Test
+		@Order(4)
+		public void testDelete() throws JsonMappingException, JsonProcessingException {
+			
+			given().spec(specification)
+				.contentType(TestConfigs.CONTENT_TYPE_JSON)
+					.pathParam("id", person.getId())
+					.when()
+					.delete("{id}")
+				.then()
+					.statusCode(204);	//código esperado
+		}
+		
+
+		@Test
+		@Order(5)
+		public void testFindAll() throws JsonMappingException, JsonProcessingException {
+		
+		var content = given().spec(specification)
+				.contentType(TestConfigs.CONTENT_TYPE_JSON)
+					.when()
+					.get()
+				.then()
+					.statusCode(200)
+						.extract()
+						.body()
+						.asString();
+							//.as(new TypeRef<List<PersonDTO>>() {});
+						
+		List <PersonDTO> people = objectMapper.readValue(content, new TypeReference<List<PersonDTO>>() {});				
+		
+		PersonDTO foundPersonOne = people.get(0);
+		
+		assertNotNull(foundPersonOne.getId());
+		assertNotNull(foundPersonOne.getFirstName());
+		assertNotNull(foundPersonOne.getLastName());
+		assertNotNull(foundPersonOne.getAddress());
+		assertNotNull(foundPersonOne.getGender());
+		
+		assertEquals(1, foundPersonOne.getId());
+		
+		assertEquals("Leandro", foundPersonOne.getFirstName());
+		assertEquals("Costa", foundPersonOne.getLastName());
+		assertEquals("Uberlândia - Minas Gerais - Brasil", foundPersonOne.getAddress());
+		assertEquals("Male", foundPersonOne.getGender());
+		
+		PersonDTO foundPersonSix = people.get(5);
+		
+		assertNotNull(foundPersonSix.getId());
+		assertNotNull(foundPersonSix.getFirstName());
+		assertNotNull(foundPersonSix.getLastName());
+		assertNotNull(foundPersonSix.getAddress());
+		assertNotNull(foundPersonSix.getGender());
+		
+		assertEquals(6, foundPersonSix.getId());
+		
+		assertEquals("Marcos", foundPersonSix.getFirstName());
+		assertEquals("Paulo", foundPersonSix.getLastName());
+		assertEquals("Patos de Minas - Minas Gerais - Brasil", foundPersonSix.getAddress());
+		assertEquals("Male", foundPersonSix.getGender());
+	}
 
 	private void mockPerson() {
 		person.setFirstName("Nelson");
