d7c88210cb6099e51abec3fdee647eb12b9681e5
TiarleiNoeremberg <tiarlei.com@gmail.com>
2023-08-28 15:56:26 -0300

    Retornando um PagedModel em Person findAll

--- 18_QueryParams e Busca Paginada/rest-spring-java-erudio/src/main/java/br/com/tiarlei/controllers/PersonController.java
+++ 18_QueryParams e Busca Paginada/rest-spring-java-erudio/src/main/java/br/com/tiarlei/controllers/PersonController.java
@@ -6,6 +6,8 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.domain.Sort.Direction;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.PagedModel;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -28,6 +30,7 @@ import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.persistence.Entity;
 
 //@CrossOrigin
 @RestController
@@ -58,7 +61,7 @@ public class PersonController {
 				@ApiResponse(description = "Internal Error", responseCode = "500", content = @Content)
 		}
 	)
-	public ResponseEntity<Page<PersonDTO>> findAll(
+	public ResponseEntity<PagedModel<EntityModel<PersonDTO>>> findAll(
 			@RequestParam(value = "page", defaultValue = "0") Integer page,
 			@RequestParam(value = "size", defaultValue = "12") Integer size,
 			@RequestParam(value = "direction", defaultValue = "asc") String direction
